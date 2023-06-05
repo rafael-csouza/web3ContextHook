d9ab00cadf8a6d0c3d56bb3a1f77617700218dc7
Bilet <rafaelcarvalhosouza@hotmail.com.br>
2023-06-05 10:49:05 -0300

    Update

--- src/pages/api/character/[search].js
+++ src/pages/api/character/[search].js
@@ -1,3 +1,4 @@
+// Just an example API
 export default async function handler(req, res) {
   const { search } = req.query;
   console.log('search:', search);
@@ -5,12 +6,9 @@ export default async function handler(req, res) {
   try {
     const response = await fetch(`https://api.tibiadata.com/v3/character/${search}`);
     const data = await response.json();
-    // console.log('data:', data);
-    console.log('test1:', data.characters.character.name);
     const { character } = data.characters;
     const { name, sex, vocation, level, account_status, comment } = character;
 
-    // Combinar os dados da API externa com dados personalizados
     const customData = {
       name,
       sex,
@@ -23,6 +21,6 @@ export default async function handler(req, res) {
     res.status(200).json(customData);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Erro ao obter os dados do personagem.' });
+    res.status(500).json({ message: 'Error getting character data.' });
   }
 }
